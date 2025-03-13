ede3032e90474520a4da03734403087b03bb7fd0
Maquiz1 <manquiz92@gmail.com>
2025-03-13 15:56:31 +0300

    Fix LAB CLINIC

--- index1.php
+++ index1.php
@@ -4,6 +4,7 @@ $user = new User();
 $override = new OverideData();
 $email = new Email();
 $random = new Random();
+$numRec = 10;
 
 $users = $override->getData('user');
 if (Input::exists('post')) {
@@ -61,7 +62,8 @@ if ($user->isLoggedIn()) {
   <title>Nimreegenin Database | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
@@ -106,10 +108,10 @@ if ($user->isLoggedIn()) {
         if ($user->data()->accessLevel == 1 || $user->data()->accessLevel == 3) {
           $Site = ' ALL SITES';
           if ($_GET['site_id']) {
-            $Site = ' ' . ' '. $override->getNews('site', 'status', 1, 'id', $_GET['site_id'])[0]['name'];
+            $Site = ' ' . ' ' . $override->getNews('site', 'status', 1, 'id', $_GET['site_id'])[0]['name'];
           }
         } else {
-          $Site =' ' . ' ' . $override->getNews('site', 'status', 1, 'id', $user->data()->site_id)[0]['name'];
+          $Site = ' ' . ' ' . $override->getNews('site', 'status', 1, 'id', $user->data()->site_id)[0]['name'];
         }
         ?>
         <div class="container-fluid">
@@ -121,7 +123,7 @@ if ($user->isLoggedIn()) {
 
               <?php
               if ($user->data()->accessLevel == 1 || $user->data()->accessLevel == 3) {
-              ?>
+                ?>
                 <form id="validation" enctype="multipart/form-data" method="post" autocomplete="off">
                   <div class="row">
                     <div class="col-sm-6">
@@ -177,7 +179,8 @@ if ($user->isLoggedIn()) {
                   <i class="ion ion-bag"></i>
                 </div>
                 <!-- <a href="info.php?id=3&site_id=<?= $user->data()->site_id; ?>&status=1" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-                <a href="info.php?id=3&status=1" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="info.php?id=3&status=1" class="small-box-footer">More info <i
+                    class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -193,7 +196,8 @@ if ($user->isLoggedIn()) {
                   <i class="ion ion-stats-bars"></i>
                 </div>
                 <!-- <a href="info.php?id=3&site_id=<?= $user->data()->site_id; ?>&status=2" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-                <a href="info.php?id=3&status=2" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="info.php?id=3&status=2" class="small-box-footer">More info <i
+                    class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -209,7 +213,8 @@ if ($user->isLoggedIn()) {
                   <i class="ion ion-person-add"></i>
                 </div>
                 <!-- <a href="info.php?id=3&site_id=<?= $user->data()->site_id; ?>&status=3" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-                <a href="info.php?id=3&status=3" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="info.php?id=3&status=3" class="small-box-footer">More info <i
+                    class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -225,7 +230,8 @@ if ($user->isLoggedIn()) {
                   <i class="ion ion-pie-graph"></i>
                 </div>
                 <!-- <a href="info.php?id=3&site_id=<?= $user->data()->site_id; ?>&status=4" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-                <a href="info.php?id=3&status=4" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="info.php?id=3&status=4" class="small-box-footer">More info <i
+                    class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -234,6 +240,138 @@ if ($user->isLoggedIn()) {
 
           <!-- Main row -->
           <div class="row">
+
+            <section class="content">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="card">
+                      <div class="card-header">
+                        <h3 class="card-title">Today Schedule</h3>
+                        <div class="card-tools">
+                          <ul class="nav nav-pills ml-auto">
+                            <li class="nav-item">
+                              <a class="nav-link" href="add.php?id=4&status=5.php"><i class="fas fa-plus"></i> Register
+                                New Client</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="info.php?id=3&status=5.php"><i class="fas fa-edit"></i>
+                                Registered Clients</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="today.php"><i class="fas fa-download"></i></a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#"><i class="fas fa-paperclip"></i></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                              <a class="nav-link" data-toggle="dropdown" href="#"><i class="fas fa-cogs"></i></a>
+                              <div class="dropdown-menu dropdown-menu-right">
+                                <a href="#" class="dropdown-item"><i class="fas fa-plus"></i> New document</a>
+                                <a href="#" class="dropdown-item"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="dropdown-item"><i class="fas fa-trash"></i> Delete</a>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="card-body">
+                        <?php if ($user->data()->power == 1) {
+                          $visits = $override->getNews('visit', 'expected_date', date('Y-m-d'), 'status', 0);
+                        } else {
+                          $visits = $override->getNews('visit', 'expected_date', date('Y-m-d'), 'status', 0);
+                          // $visits = $override->get3('visit', 'expected_date', date('Y-m-d'), 'status', 0, 'site_id',$user->data()->site_id);
+                        } ?>
+                        <form method="post">
+                          <button type="submit" name="today_schedule" class="btn btn-primary mb-3">Download
+                            Excel</button>
+                        </form>
+                        <div class="table-responsive">
+                          <table class="table table-bordered table-striped">
+                            <thead>
+                              <tr>
+                                <th><input type="checkbox" name="checkall" /></th>
+                                <th>#</th>
+                                <th>Picture</th>
+                                <th>Study ID</th>
+                                <th>ON NIMREGENIN</th>
+                                <th>Visit Name</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Age</th>
+                                <th>Site</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <?php
+                              $x = 1;
+                              foreach ($visits as $visit) {
+                                $client = $override->get3('clients', 'id', $visit['client_id'], 'enrolled', 1, 'end_study', 0)[0];
+                                $site = $override->getNews('clients', 'id', $visit['client_id'], 'status', 1)[0]['site_id'];
+                                $site_id = $override->get('site', 'id', $site)[0]['name'];
+                                ?>
+                                <tr>
+                                  <td><input type="checkbox" name="checkbox" /></td>
+                                  <td><?= $x ?></td>
+                                  <td>
+                                    <?php
+                                    $img = $client['client_image'] != '' || is_null($client['client_image']) ? $client['client_image'] : 'img/users/blank.png';
+                                    ?>
+                                    <a href="#img<?= $client['id'] ?>" data-toggle="modal"><img src="<?= $img ?>"
+                                        width="90" height="90" class="img-thumbnail" /></a>
+                                  </td>
+                                  <td><?= $client['study_id'] ?></td>
+                                  <td>
+                                    <a href="#"
+                                      class="btn <?= $client['nimregenin'] == 1 ? 'btn-info' : 'btn-warning' ?>">
+                                      <?= $client['nimregenin'] == 1 ? 'YES' : 'NO' ?>
+                                    </a>
+                                  </td>
+                                  <td><?= $visit['visit_name'] ?></td>
+                                  <td><?= $client['firstname'] . ' ' . $client['lastname'] ?></td>
+                                  <td><?= $client['gender'] ?></td>
+                                  <td><?= $client['age'] ?></td>
+                                  <td><?= $site_id ?></td>
+                                  <td>
+                                    <a href="info.php?id=7&cid=<?= $client['id'] ?>" class="btn btn-warning">Schedule</a>
+                                  </td>
+                                </tr>
+                                <div class="modal fade" id="img<?= $client['id'] ?>" tabindex="-1" role="dialog"
+                                  aria-labelledby="myModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <form method="post">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <button type="button" class="close" data-dismiss="modal"><span
+                                              aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                          <h4 class="modal-title">Client Image</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                          <img src="<?= $img ?>" width="350" class="img-fluid">
+                                        </div>
+                                        <div class="modal-footer">
+                                          <button class="btn btn-default" data-dismiss="modal"
+                                            aria-hidden="true">Close</button>
+                                        </div>
+                                      </div>
+                                    </form>
+                                  </div>
+                                </div>
+                                <?php $x++;
+                              } ?>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- /.container-fluid -->
+            </section>
             <!-- Left col -->
             <section class="col-lg-6 connectedSortable">
               <!-- Custom tabs (Charts with tabs)-->
@@ -266,9 +404,9 @@ if ($user->isLoggedIn()) {
                     </div>
                   </div>
                 </div> -->
-                <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
 
             </section>
             <!-- /.Left col -->
@@ -488,10 +626,6 @@ if ($user->isLoggedIn()) {
   <script src="dist/js/pages/dashboard1_4.js"></script>
   <script src="dist/js/pages/dashboard1_5.js"></script>
   <script src="dist/js/pages/dashboard1_6.js"></script>
-
-
-
-
 
   <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
 
